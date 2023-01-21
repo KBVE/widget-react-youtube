5e8d50b27802292c8cb4c447a4a39078d43a22df
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-01-20 23:30:11 -0500

    Saving
    [1] Migrating from a general YT video loader to a larger / abstract video
    [2] I believe the library choice will be ReactVideo, which will increase the bundle size but expand our selection of possible videos that we can play.
    [3] Ideally integrating the Humix into the Player would be the end goal.

--- src/App.js
+++ src/App.js
@@ -19,12 +19,13 @@ import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import Masonry from "@mui/lab/Masonry";
 
+//*       YT
+import YT from './YT';
 
 function App({kbve_dom_element}) {
   const _yt = kbve_dom_element.getAttribute("yt-id");
   const _ytJSON = kbve_dom_element.getAttribute("yt-json");
 
-  const [youtubeId, setYoutubeId] = React.useState();
   const [loading, setLoading] = React.useState();
   const [error, setError] = React.useState("");
   const [data, setData] = React.useState([]);
@@ -43,41 +44,12 @@ function App({kbve_dom_element}) {
 
   function renderPost(post) {
     const {
-      slug,
       data: { title, id, description, img, author_name },
     } = post;
-
     return (
       <Paper item xs={4} key={id}>
         <Card>
-          <CardMedia sx={{ height: 140 }} image={img} title={title} />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              <Link
-                href={`https://kbve.com/${slug}/`}
-                underline="hover"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {title} by {author_name}
-              </Link>
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {description}
-            </Typography>
-          </CardContent>
-          <Divider />
-          <CardActions sx={{ justifyContent: "center" }}>
-            <Button variant="contained" sx={{ m: 1 }} size="small">
-              KBVE
-            </Button>
-            <Button variant="contained" sx={{ m: 1 }} size="small">
-              Share
-            </Button>
-            <Button variant="contained" sx={{ m: 1 }} size="small">
-              Learn More
-            </Button>
-          </CardActions>
+         {title}
         </Card>
       </Paper>
     );
@@ -89,9 +61,7 @@ function App({kbve_dom_element}) {
       .then((response) => response.json())
       .then((data) => {
         setLoading(false);
-        console.log(data);
-        //setYoutubeId(data.slice(0, _limit));
-        //setYoutubeId(data);
+        //console.log(data);
         setData(data);
       })
       .catch((e) => {
@@ -102,18 +72,21 @@ function App({kbve_dom_element}) {
   }, [_yt, _ytJSON]);
 
   
-  function noDraft(post) {
-    return !post.data.draft;
-  }
+// function noDraft(post) {  return !post.data.draft;  }
 
   return (
     <>
       <ThemeProvider theme={theme}>
+        <Card>
+          {_yt}
+        </Card>
+       
         <Grid fluid="md">
           {loading && "Loading..."}
           {error && error}
           {!!data.length && (
-            <Masonry>{data.filter(noDraft).map(renderPost)}</Masonry>
+            
+            <Divider>{data.map(renderPost)}</Divider>
           )}
         </Grid>
       </ThemeProvider>
